744d239a18b0d33881c00d0d610794c273704b34
Andrey <ashkrebela@gmail.com>
2020-07-27 16:18:00 +0300

    Created team, collaboration and blog section

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 mix.config.publicPath='dist';
+mix.config.sourcemaps='true';
+console.log(mix.config)
 mix.js('src/js/main.js', 'dist/js')
     .sass('src/sass/main.scss', 'dist/css')
     .copyDirectory('src', 'dist');
