b3ab27a3b03ef3b0bb09640e7fc3c0aaed1fc00b
Andrey <ashkrebela@gmail.com>
2020-07-29 18:25:59 +0300

    Cleaned up the project and rewritten by BEM (not completely)

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import Swiper, { Navigation, Pagination, Mousewheel } from 'swiper';
+import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
 
-// configure Swiper to use modules
 Swiper.use([Navigation, Pagination, Mousewheel]);
 
-var swiper1 = new Swiper('.primary-screen__swiper-container', {
+let swiperFirst;
+swiperFirst = new Swiper('.primary-screen__swiper-container', {
     direction: 'horizontal',
     loop: true,
     navigation: {
@@ -12,7 +12,8 @@ var swiper1 = new Swiper('.primary-screen__swiper-container', {
     },
 });
 
-var swiper2 = new Swiper('.about-company__swiper-container', {
+let swiperSecond;
+swiperSecond = new Swiper('.about-company__swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
     spaceBetween: 30,
@@ -23,7 +24,8 @@ var swiper2 = new Swiper('.about-company__swiper-container', {
     },
 });
 
-var swiper3 = new Swiper('.case-study__swiper-container', {
+let swiperThird;
+swiperThird = new Swiper('.case-study__swiper-container', {
     direction: 'horizontal',
     slidesPerView: 1,
     spaceBetween: 30,
@@ -48,18 +50,18 @@ menuToggle.addEventListener('click', () => {
     toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
 });
 
-    Array.from(menuLink).forEach(i => {
-        i.addEventListener('click', () => {
-            toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
-            toggleClass(menu, 'main-header__menu--active-menu-toggle');
-            toggleClass(body, 'body--stop-scrolling');
-            toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
-        });
+Array.from(menuLink).forEach(i => {
+    i.addEventListener('click', () => {
+        toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
+        toggleClass(menu, 'main-header__menu--active-menu-toggle');
+        toggleClass(body, 'body--stop-scrolling');
+        toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
     });
+});
 
-    function toggleClass(el, className) {
-        el.classList.toggle(className);
-    }
+function toggleClass(el, className) {
+    el.classList.toggle(className);
+}
 
 
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,9 @@ const mix = require('laravel-mix');
 mix.config.publicPath='dist';
 mix.config.sourcemaps='true';
 console.log(mix.config)
-mix.js('src/js/main.js', 'dist/js')
+mix.js('src/js/main.js', 'dist/js/index.js')
     .sass('src/sass/main.scss', 'dist/css')
-    .copyDirectory('src', 'dist');
+    .copyDirectory('src/font', 'dist/font')
+    .copyDirectory('src/img', 'dist/img')
+    .copy('src/index.html', 'dist/index.html');
 
